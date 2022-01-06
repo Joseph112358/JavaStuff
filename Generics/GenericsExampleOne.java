491010aa7ee499e859de05b09d12a7345eb6e5aa
Joe Shaw <63563424+Joseph112358@users.noreply.github.com>
2022-01-06 23:14:44 +0000

    GenericsExampleOne

---
+++ Generics/GenericsExampleOne.java
@@ -0,0 +1,56 @@
+
+import java.util.StringJoiner;
+//T represents the type (it could be an int,string or yet undefined classes
+ class Pair<T>{
+  private final T first;
+  private final T second;
+
+  public Pair(T first,T second ){
+      this.first = first;
+      this.second = second;
+  }
+
+  public T getFirst(){
+      return first;
+  }
+
+  public T getSecond(){
+      return second;
+  }
+  @Override
+     public String toString(){
+      return new StringJoiner(",", Pair.class.getSimpleName()+"[","]")
+              .add("first " + first + " ")
+              .add("second " + second + " ")
+              .toString();
+
+  }
+}
+
+public class Main {
+
+    public static void main(String []args){
+        //Strings
+        Pair<String> mypair = new Pair("one","two");
+        System.out.println(mypair);
+        //Integers
+        Pair<Integer> intpair = new Pair<>(1,2);
+        System.out.println(intpair);
+
+        //Animals!
+        Animal sheep = new Animal("Sheep");
+        Animal dog = new Animal("Dog");
+        Pair<Animal> animalPair = new Pair<>(sheep,dog);
+        System.out.println(animalPair);
+        //in the animal situation we may want to consider using an interface so we can define how
+        //the getString method works, perhaps we may want it to output the animals species.
+    }
+
+}
+
+class Animal{
+     private String species;
+     public Animal(String species){
+       this.species = species;
+     }
+}
